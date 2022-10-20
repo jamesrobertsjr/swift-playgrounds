34a3b0d1a6305b32eb8136dcecc764697daec06c
jameserv <106282058+jameserv@users.noreply.github.com>
2022-10-20 14:33:11 -0400

    Add findAndFixBugs.swift

---
+++ src/ltc1/commands/findAndFixBugs.swift
@@ -0,0 +1,19 @@
+/* Goal: Find the bugs and fix them.
+
+When you write code, it's easy to make mistakes. A mistake that keeps your program from running correctly is called a
+bug, and finding and fixing bugs is called debugging.
+
+The code below contains one or more bugs. To debug it, rearrange the commands into the right order to solve the puzzle.
+
+1. Run the code to see where the mistake occurs.
+2. Identify the command that's in the wrong place, then press the command to select it.
+3. Drag the command to the correct location, then run the code again to test it.
+*/
+
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+collectGem()
+moveForward()
+toggleSwitch()
