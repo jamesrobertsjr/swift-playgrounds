8d068f0abce8e70a68c68fdf295a3cd37b0d3223
jameserv <106282058+jameserv@users.noreply.github.com>
2022-10-20 14:34:34 -0400

    togglingSwitch.swift

---
+++ src/ltc1/commands/togglingSwitch.swift
@@ -0,0 +1,25 @@
+/* Goal: Collect the gem, then toggle the switch.
+
+So far, you've learned how to make Byte move around and collect a gem. In this puzzle, you'll use another new command:
+toggleSwitch().
+
+Switches:
+A switch might be toggled open or closed. 
+Closed switches are off and awaiting activation.
+Open switches are on and show a light inner core.
+
+1. Enter the commands to move Byte to the gem, then collect it.
+2. Continue entering commands to move Byte to the switch, then toggle it.
+3. Run your code.
+*/
+
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+collectGem()
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+toggleSwitch()
