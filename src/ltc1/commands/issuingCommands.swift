b3871c21a8804ead8b229716245c3714876a5f47
jameserv <106282058+jameserv@users.noreply.github.com>
2022-10-20 14:33:22 -0400

    Add issuingCommands.swift

---
+++ src/ltc1/commands/issuingCommands.swift
@@ -0,0 +1,14 @@
+/* Goal: Use swift commands to tell byte to move and collect a gem.
+
+Your character, Byte, loves to collect gems but can't do it alone. In this first puzzle, you'll need to write Swift 
+commands to move Byte to move Byte across the puzzle world to collect a gem.
+
+1. Look for the gem in the puzzle world.
+2. Enter the correct combination of the moveForward() and collectGem() commands.
+3. Press Run My Code.
+*/
+
+moveForward()
+moveForward()
+moveForward()
+collectGem()
