3df58e7585731bdfeb5f4ec1e82d8d6b472b51e3
jameserv <106282058+jameserv@users.noreply.github.com>
2022-10-20 14:32:35 -0400

    Add addingNewCommand.swift

---
+++ src/ltc1/commands/addingNewCommand.swift
@@ -0,0 +1,16 @@
+/* Goal: Combine move and turn commands to collect the gem.
+
+This puzzle is just like the last one, but this time Byte needs to turn left to reach the gem. You can use the commands 
+from the previous puzzle as well as a new command: `turnLeft()`.
+
+1. Plan the commands you need to make Byte move to the gem and collect it.
+2. Enter your commands in the code area.
+3. Run your code.
+*/
+
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+collectGem()
